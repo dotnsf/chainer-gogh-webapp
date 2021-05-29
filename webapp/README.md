c40ef19dda170416d5f3b97b53c8639603e27cad
K.Kimura <dotnsf@gmail.com>
2021-05-30 04:15:12 +0900

    first commit

---
+++ webapp/README.md
@@ -0,0 +1,171 @@
+# chainer-gogh-webapp
+
+## Overview
+
+Web front-end application for [chainer-gogh](https://github.com/mattya/chainer-gogh).
+
+
+## Caution
+
+This web application(`webapp`) can be run on public server. But that is not our recommended way to execute.
+
+We recommend that you should run this application on **your local machine**. This application need huge processing resource to run, and huge amount of time to complete. Sharing is not appropriate way.
+
+
+## Setup
+
+We expect you setup `chainer-gogh` and `webapp` in same folder, like this:
+
+```
+  :
+|- chainer-gogh/
+|
+|- webapp/
+  : 
+```
+
+### chainer-gogh-webapp
+
+Web front-end of chainer-gogh.
+
+
+#### How to install chainer-gogh-web
+
+- Install Node.js and npm.
+
+- Git clone `chainer-gogh-webapp`:
+
+  - https://github.com/dotnsf/chainer-gogh-webapp
+
+- Install dependent libraries:
+
+  - `$ cd chainer-gogh-webapp`
+
+  - `$ npm install`
+
+
+#### How to run chainer-gogh-web
+
+- Run with node.js without GPU:
+
+  - `$ node app`
+
+- Run with node.js with GPU:
+
+  - `$ GPU=1 node app`
+
+
+#### How to use chainer-gogh-web
+
+- Access to web page:
+
+  - http://localhost:8080/
+
+- Input MyDoodle ID as style image.
+
+  - Transparent background would be regarded as `black`.
+
+- Specify image file as main image.
+
+- Click `RESULT` button.
+
+
+### chainer-gogh
+
+Backend main module of this application.
+
+
+#### How to install chainer-gogh
+
+- Install Python 3.x and pip.
+
+- Install Anaconda:
+
+  - https://www.kkaneko.jp/tools/ubuntu/anaconda.html
+
+- Install chainer with pip:
+
+  - `$ pip install chainer`
+
+- Install pillow with conda:
+
+  - `$ conda install -c anaconda pillow`
+
+- Git clone `chainer-gogh`:
+
+  - `$ cd chainer-gogh-webapp`
+
+  - `$ git clone https://github.com/mattya/chainer-gogh`
+
+- Download nin model, and put it to chainer-gogh foler:
+
+  - https://www.dropbox.com/s/cphemjekve3d80n/nin_imagenet.caffemodel?dl=1
+
+- (Optional)Download vgg model, and put it to chainer-gogh foler, **if you want to run with GPU**:
+
+  - http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_16_layers.caffemodel
+
+- Edit some files for recent environment:
+
+  `chainer-gogh.py`
+  ```
+   13 : - from chainer.functions import caffe
+   13 : + from chainer.links import caffe
+
+  100 : - mid_orig = nn.forward(Variable(img_orig, volatile=True))
+  101 : - style_mats = [get_matrix(y) for y in nn.forward(Variable(img_style, volatile=True))]
+  100 : + with chainer.using_config("enable_backprop", False):
+  101 : +     mid_orig = nn.forward(Variable(img_orig))
+  102 : + with chainer.using_config("enable_backprop", False):
+  103 : +     style_mats = [get_matrix(y) for y in nn.forward(Variable(img_style))]
+  ```
+
+  `models.py`
+  ```
+    5 : - from chainer.functions import caffe
+    5 : + from chainer.links import caffe
+
+   22 : - x3 = F.relu(getattr(self.model,"conv4-1024")(F.dropout(F.average_pooling_2d(F.relu(y3), 3, stride=2), train=False)))
+   22 : + x3 = F.relu(getattr(self.model,"conv4-1024")(F.dropout(F.average_pooling_2d(F.relu(y3), 3, stride=2))))
+
+   74 : - y6 = self.model.conv6_4(F.relu(F.dropout(self.model.conv6_3(F.relu(self.model.conv6_2(F.relu(self.model.conv6_1(x5))))), train=False)))
+   74 : + y6 = self.model.conv6_4(F.relu(F.dropout(self.model.conv6_3(F.relu(self.model.conv6_2(F.relu(self.model.conv6_1(x5))))))))
+  ```
+
+
+#### How to run chainer-gogh with CPU
+
+- Put main image(main.png) into chainer-gogh folder.
+
+- Put sub image(sub.png) into chainer-gogh folder.
+
+- Run following command (to output into output_dir/ folder):
+
+  - `$ python chainer-gogh.py -m nin -i main.png -s sub.png -o output_dir -g -1`
+
+
+#### How to run chainer-gogh with CPU
+
+- Run following command (to output into output_dir/ folder):
+
+  - `$ python chainer-gogh.py -m vgg -i main.png -s sub.png -o output_dir -g 0`
+
+
+## References
+
+https://chime-note.hatenablog.com/entry/chainer-gogh-cat-sakura
+
+https://chime-note.hatenablog.com/entry/chainer-gogh-using-gpu
+
+https://qiita.com/virtual_techX/items/d234d063cd638f4daa59
+
+
+## Licensing
+
+This code is licensed under MIT.
+
+
+## Copyright
+
+2021 K.Kimura @ Juge.Me all rights reserved.
+
---
+++ webapp/app.js
@@ -0,0 +1,130 @@
+//. app.js
+var express = require( 'express' ),
+    fs = require( 'fs' ),
+    multer = require( 'multer' ),
+    bodyParser = require( 'body-parser' ),
+    { exec } = require( 'child_process' ),
+    app = express();
+
+var use_gpu = ( process.env.GPU ? true : false );
+var chainer_gogh_folder = __dirname + '/../chainer-gogh/';
+var sub_command = 'wget https://mydoodles.mybluemix.net/attachment/';
+//var main_command = 'python ' + chainer_gogh_folder + 'chainer-gogh.py -m ' + ( use_gpu ? 'vgg' : 'nin' ) + ' -g ' + ( use_gpu ? '0' : '-1' ) + ' -o ' + chainer_gogh_folder + 'output_dir/';
+var main_command = 'cd ' + chainer_gogh_folder + ' && python chainer-gogh.py -m ' + ( use_gpu ? 'vgg' : 'nin' ) + ' -g ' + ( use_gpu ? '0' : '-1' ) + ' -o output_dir/';
+
+app.use( multer( { dest: './tmp/' } ).single( 'image' ) );
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json() );
+app.use( express.static( __dirname + '/public' ) );
+
+app.post( '/image', function( req, res ){
+  res.contentType( 'application/json; charset=utf-8' );
+
+  if( req.file && req.file.path ){
+    if( req.body.sub_id ){
+      var sub_id = req.body.sub_id;
+      var wget_command = sub_command + sub_id + ' -O ' + __dirname + '/tmp/' + sub_id + '.png';
+      console.log( { wget_command } );
+      exec( wget_command, function( err0, result0, stderr0 ){
+        if( err0 ){
+          console.log( { err0 } );
+          res.status( 400 );
+          res.write( JSON.stringify( { status: false, error: err0 } ) );
+          res.end();
+          /*
+        }else if( stderr0 ){
+          console.log( { stderr0 } );
+          res.status( 400 );
+          res.write( JSON.stringify( { status: false, error: stderr0 } ) );
+          res.end();
+          */
+        }else{
+          //console.log( { result0 } );
+          var python_command = main_command + sub_id + ' -i ' + __dirname + '/' + req.file.path + ' -s ' + __dirname + '/tmp/' + sub_id + '.png';
+          console.log( { python_command } );
+
+          exec( python_command, function( err1, result1, stderr1 ){
+            if( err1 ){
+              console.log( { err1 } );
+              res.status( 400 );
+              res.write( JSON.stringify( { status: false, error: err1 } ) );
+              res.end();
+            }else if( stderr1 ){
+              console.log( { stderr1 } );
+              res.status( 400 );
+              res.write( JSON.stringify( { status: false, error: stderr1 } ) );
+              res.end();
+            }else{
+              console.log( { result1 } );
+              res.write( JSON.stringify( { status: true, result: result1 } ) );
+              res.end();
+            }
+          });
+
+          setTimeout( function(){
+            fs.unlink( req.file.path, function( err ){} );
+            fs.unlink( __dirname + '/tmp/' + sub_id, function( err ){} );
+
+            //. これを返したら上の python プロセスは止まる？
+            //. ノンブロッキングなら返す必要ない？
+            //res.write( JSON.stringify( { status: true } ) );
+            //res.end();
+          }, 10000 );
+        }
+      });
+    }else{
+      res.status( 400 );
+      res.write( JSON.stringify( { status: false, error: 'no sub file.' } ) );
+      res.end();
+    }
+  }else{
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, error: 'no main file.' } ) );
+    res.end();
+  }
+});
+
+app.get( '/image/:sub_id', function( req, res ){
+  var sub_id = req.params.sub_id;
+  if( sub_id ){
+    //var chainer_gogh_folder = __dirname + '/../chainer-gogh/';
+    var target_folder = chainer_gogh_folder + 'output_dir/' + sub_id;
+    fs.readdir( target_folder, function( err, files ){
+      if( err ){
+        res.contentType( 'application/json; charset=utf-8' );
+        res.status( 400 );
+        res.write( JSON.stringify( { status: false, error: err } ) );
+        res.end();
+      }else{
+        var latest = '';
+        files.filter( function( file ){
+          return fs.statSync( target_folder + '/' + file ).isFile();
+        }).forEach( function( file ){
+          if( file > latest ){
+            latest = file;
+          }
+        });
+
+        if( latest ){
+          res.contentType( 'image/png' );
+          var body = fs.readFileSync( target_folder + '/' + latest );
+          res.end( body, 'binary' );
+        }else{
+          res.contentType( 'application/json; charset=utf-8' );
+          res.status( 400 );
+          res.write( JSON.stringify( { status: false, error: 'no file generated yet.' } ) );
+          res.end();
+        }
+      }
+    });
+  }else{
+    res.contentType( 'application/json; charset=utf-8' );
+    res.status( 400 );
+    res.write( JSON.stringify( { status: false, error: 'no sub_id.' } ) );
+    res.end();
+  }
+});
+
+var port = process.env.PORT || 8080;
+app.listen( port );
+console.log( "server starting on " + port + " ..." );
