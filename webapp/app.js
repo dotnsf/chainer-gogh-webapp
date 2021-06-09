555d14d5b83d19ba6af6a28e5730b0fd9dbaa7e0
K.Kimura <dotnsf@gmail.com>
2021-06-09 15:02:19 +0900

    path.sep applied.

--- webapp/app.js
+++ webapp/app.js
@@ -3,20 +3,23 @@ var express = require( 'express' ),
     fs = require( 'fs' ),
     multer = require( 'multer' ),
     bodyParser = require( 'body-parser' ),
+    path = require( 'path' ),
     { exec } = require( 'child_process' ),
     app = express();
+
+var sep = path.sep;
 
 var env_python = ( process.env.python ? process.env.python : 'python' );
 var env_gpu = ( process.env.GPU ? true : false );
 var env_model = ( process.env.model ? process.env.model : 'nin' );
 var env_lam = ( process.env.lam ? process.env.lam : 0.5 );  //. #5 デフォルトは 0.005
-var chainer_gogh_folder = __dirname + '/../chainer-gogh/';
+var chainer_gogh_folder = __dirname + sep + '..' + sep + 'chainer-gogh' + sep;
 var sub_command = 'wget https://mydoodles.mybluemix.net/attachment/';
 
-app.use( multer( { dest: './tmp/' } ).single( 'image' ) );
+app.use( multer( { dest: '.' + sep + 'tmp' + sep } ).single( 'image' ) );
 app.use( bodyParser.urlencoded( { extended: true } ) );
 app.use( bodyParser.json() );
-app.use( express.static( __dirname + '/public' ) );
+app.use( express.static( __dirname + sep + 'public' ) );
 
 var processing = false;
 app.post( '/image', function( req, res ){
@@ -31,7 +34,7 @@ app.post( '/image', function( req, res ){
       if( req.body.sub_id ){
         processing = true;
         var sub_id = req.body.sub_id;
-        var wget_command = sub_command + sub_id + ' -O ' + __dirname + '/tmp/' + sub_id + '.png';
+        var wget_command = sub_command + sub_id + ' -O ' + __dirname + sep + 'tmp' + sep + sub_id + '.png';
         console.log( { wget_command } );
         exec( wget_command, function( err0, result0, stderr0 ){
           if( err0 ){
@@ -49,8 +52,8 @@ app.post( '/image', function( req, res ){
             */
           }else{
             //console.log( { result0 } );
-            var main_command = 'cd ' + chainer_gogh_folder + ' && ' + env_python + ' chainer-gogh.py' + ( env_lam ? ' --lam ' + env_lam : '' ) +  ' -m ' + env_model + ' -g ' + ( env_gpu ? '0' : '-1' ) + ' -o output_dir/';
-            var python_command = main_command + sub_id + ' -i ' + __dirname + '/' + req.file.path + ' -s ' + __dirname + '/tmp/' + sub_id + '.png';
+            var main_command = 'cd ' + chainer_gogh_folder + ' && ' + env_python + ' chainer-gogh.py' + ( env_lam ? ' --lam ' + env_lam : '' ) +  ' -m ' + env_model + ' -g ' + ( env_gpu ? '0' : '-1' ) + ' -o output_dir' + sep;
+            var python_command = main_command + sub_id + ' -i ' + __dirname + sep + req.file.path + ' -s ' + __dirname + sep + 'tmp' + sep + sub_id + '.png';
             console.log( { python_command } );
   
             exec( python_command, function( err1, result1, stderr1 ){
@@ -76,7 +79,7 @@ app.post( '/image', function( req, res ){
   
             setTimeout( function(){
               fs.unlink( req.file.path, function( err ){} );
-              fs.unlink( __dirname + '/tmp/' + sub_id, function( err ){} );
+              fs.unlink( __dirname + sep + 'tmp' + sep + sub_id, function( err ){} );
   
               //. これを返したら上の python プロセスは止まる？
               //. ノンブロッキングなら返す必要ない？
@@ -104,7 +107,7 @@ app.get( '/image/:sub_id', function( req, res ){
   var sub_id = req.params.sub_id;
   if( sub_id ){
     //var chainer_gogh_folder = __dirname + '/../chainer-gogh/';
-    var target_folder = chainer_gogh_folder + 'output_dir/' + sub_id;
+    var target_folder = chainer_gogh_folder + 'output_dir' + sep + sub_id;
     fs.readdir( target_folder, function( err, files ){
       if( err ){
         res.contentType( 'application/json; charset=utf-8' );
@@ -114,7 +117,7 @@ app.get( '/image/:sub_id', function( req, res ){
       }else{
         var latest = '';
         files.filter( function( file ){
-          return fs.statSync( target_folder + '/' + file ).isFile();
+          return fs.statSync( target_folder + sep + file ).isFile();
         }).forEach( function( file ){
           if( file > latest ){
             latest = file;
@@ -123,7 +126,7 @@ app.get( '/image/:sub_id', function( req, res ){
 
         if( latest ){
           res.contentType( 'image/png' );
-          var body = fs.readFileSync( target_folder + '/' + latest );
+          var body = fs.readFileSync( target_folder + sep + latest );
           res.end( body, 'binary' );
         }else{
           res.contentType( 'application/json; charset=utf-8' );
@@ -147,8 +150,8 @@ app.get( '/finished/:sub_id', function( req, res ){
   var sub_id = req.params.sub_id;
   if( sub_id ){
     //var chainer_gogh_folder = __dirname + '/../chainer-gogh/';
-    var target_folder = chainer_gogh_folder + 'output_dir/' + sub_id;
-    var exist = fs.existsSync( target_folder + '/im_4950.png' );
+    var target_folder = chainer_gogh_folder + 'output_dir' + sep + sub_id;
+    var exist = fs.existsSync( target_folder + sep + 'im_4950.png' );
     res.write( JSON.stringify( { status: !exist } ) );
     res.end();
   }else{
